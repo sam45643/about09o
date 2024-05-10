fe57dbbd54ada97a6a5a129ceba0a89f7e88245e
sam45643 <122574805+sam45643@users.noreply.github.com>
2024-05-10 16:39:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;} // Made by Yago Estévez (Twitter: @yagoestevez.com)
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;} // Made by Yago Estévez (Twitter: @yagoestevez.com)
 
 
 /***********************
@@ -221,6 +221,42 @@ const Projects = props => {
     React.createElement("u", null, "most"), " of the projects I've been working on lately. All of these were built during my coding camp adventure on",
     ' ', /*#__PURE__*/
     React.createElement("a", { href: "https://www.freecodecamp.org/", target: "_blank", rel: "noopener noreferrer" }, "freeCodeCamp"), ", where I've been coding for almost 7 months non-stop until I completed all the projects required to get my Full-Stack Developer certification.")), /*#__PURE__*/
+
+
+
+
+
+
+    React.createElement("div", { className: "projects-wrapper" }, /*#__PURE__*/
+    React.createElement(Project, {
+      title: "Anonymous Message Board.",
+      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/AnonMsgBoard.jpg',
+      tech: "js css react node",
+      link: "https://yagoestevez-anon-msg-board.glitch.me/",
+      repo: "https://github.com/yagoestevez/anonymous-message-board" }, /*#__PURE__*/
+
+    React.createElement("small", null, "Built using Node, Express, MongoDB, CSS + Bulma, React.js and React Router."), /*#__PURE__*/
+
+
+    React.createElement("p", null, "This is a full-stack website that I made which lets the users read and post anonymous messages and replies.")), /*#__PURE__*/
+
+
+
+
+    React.createElement(Project, {
+      title: "Stock Price Checker.",
+      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/StockPriceChecker.jpg',
+      tech: "js node css",
+      link: "https://yagoestevez-stock-price-checker.glitch.me/",
+      repo: "https://github.com/yagoestevez/fcc-stock-price-checker" }, /*#__PURE__*/
+
+    React.createElement("small", null, "Built using Node, Express, MongoDB, JS + jQuery + Axios, CSS + Bootstrap and Pug."), /*#__PURE__*/
+
+
+    React.createElement("p", null, "Another full-stack website to check the current value of any requested stock in the market.")), /*#__PURE__*/
+
+
+
 
     React.createElement(Project, {
       title: "Personal Library.",
