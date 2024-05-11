02c3df237bbe448efbd00f2e944e9f2fdcef1c5a
sam45643 <122574805+sam45643@users.noreply.github.com>
2024-05-10 22:53:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-js
-
 function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;} // Made by Yago Estévez (Twitter: @yagoestevez.com)
 
 
